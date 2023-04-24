1552b8933e2e0b3923dadfceab8034884e2dd713
Shavkat Berdibekov <103271898+shavkat9@users.noreply.github.com>
2023-04-24 14:29:30 +0500

    method filter

---
+++ Новая папка/app.js
@@ -0,0 +1,19 @@
+// const arr = [6, 3, 2, 7, 3]
+
+
+// function isEven(x) {
+//      return x % 2 === 0;
+// }
+// const output = arr.filter(isEven);
+
+// console.log(output);
+
+const arr = [ 6, 2, 7, 3, 9, 5]
+
+
+function isOdd (x){
+     return x % 2 
+}
+const output = arr.filter(isOdd);
+
+console.log(output)
